108706461e65c9f556e17819eee98de5bfeddd12
Haiman Nath <84642708+nAthHaiman@users.noreply.github.com>
2021-08-01 23:05:23 +0600

    Create V.c

---
+++ V.c
@@ -0,0 +1,35 @@
+//prepared by haiman
+
+#include<stdio.h>
+ 
+int main()
+{
+    int num1, num2;
+    char ch;
+    scanf("%d %c %d", &num1, &ch,  &num2);
+ 
+    if(ch == '<')
+    {
+ 
+        if(num1 < num2)
+           printf("Right\n");
+        else
+            printf("Wrong\n");
+    }
+    else  if(ch == '>')// num1<=num2
+    {
+ 
+        if(num1 > num2)
+            printf("Right\n");
+        else
+             printf("Wrong\n");
+    }
+    else
+    {
+        if (num1 == num2)
+            printf("Right\n");
+        else
+            printf("Wrong\n");
+    }
+    return 0;
+}
